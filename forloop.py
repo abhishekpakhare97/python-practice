1a501189b5f7952a97fb8c74589f8651805165cd
abhishekpakhare97 <abhishekpakhare1997@gmail.com>
2019-06-26 19:00:02 +0530

    this contain code for FOR LOOP

---
+++ forloop.py
@@ -0,0 +1,25 @@
+# for item in "Abhishek":
+#     print(item)
+
+# for name in ['abhishek', 'ramesh', 'pakhare']:
+#     print(name)
+
+# for range in range(20): #this will print from 0 to 19
+#     print(range)
+
+
+#To calculate total price of shopping cart
+# price = [10, 20, 30]
+# total = 0
+# for calculate in price:
+#     total += calculate
+# print(f"Total : {total}")
+
+
+numbers = [1,1,1,1,5]
+
+for count_x in numbers:
+    output = ""
+    for count in range(count_x):
+        output += "x"
+    print(output)
